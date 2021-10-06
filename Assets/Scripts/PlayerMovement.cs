523bea20fefe25d53b43b8e13479039c44f10b49
alexshko <alexshko@gmail.com>
2021-10-06 21:51:45 +0300

    .

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Vector2 mouseSpeed;
+    public float MouseAimMaxAngle = 90;
+    public float MouseAimMinAngle = -90;
     //sum the change in the mouseAim:
     private float vertMouse;
     private float horzMouse;
@@ -22,8 +24,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MakeMouseMove();
+    }
+
+    private void MakeMouseMove()
+    {
         vertMouse += Input.GetAxis("Mouse X") * mouseSpeed.x;
         horzMouse += Input.GetAxis("Mouse Y") * mouseSpeed.y;
+
+        vertMouse = Mathf.Clamp(vertMouse, MouseAimMinAngle, MouseAimMaxAngle);
+        horzMouse = Mathf.Clamp(horzMouse, MouseAimMinAngle, MouseAimMaxAngle);
+
         Debug.Log("vert:" + vertMouse);
         cam.localEulerAngles = new Vector3(-horzMouse, vertMouse, 0);
     }
