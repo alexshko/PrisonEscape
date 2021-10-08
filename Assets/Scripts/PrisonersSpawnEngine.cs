4a46200d9996bad3e63ec166358344c3e829ccbc
alexshko <alexshko@gmail.com>
2021-10-08 13:59:10 +0300

    .

---
+++ Assets/Scripts/PrisonersSpawnEngine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace alexshko.prisonescape.Prisoners
+{
+    public class PrisonersSpawnEngine : MonoBehaviour
+    {
+        //public Transform EscapeTargetsTreeRoot;
+        private GameObject[] EscapeTargets;
+
+        private void Start()
+        {
+            fIndAllEscapeTargets();
+        }
+
+        private void fIndAllEscapeTargets()
+        {
+
+            //foreach (var possibleTarget in EscapeTargetsTreeRoot.GetComponentsInChildren<Transform>())
+            //{
+            //    if (possibleTarget.tag == "EscapeTarget")
+            //    {
+            //        EscapeTargets.Add(possibleTarget);
+            //    }
+            //}
+            EscapeTargets = GameObject.FindGameObjectsWithTag("EscapeTarget");
+
+        }
+    }
+}
