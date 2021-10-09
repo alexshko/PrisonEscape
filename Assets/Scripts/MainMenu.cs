6b51d5611bae9c0be63d30dfcb5d18fbaf5ac90d
alexshko <alexshko@gmail.com>
2021-10-10 00:26:27 +0300

    .

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace alexshko.prisonescape.Core
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void LoadLevel(string name)
+        {
+
+        }
+
+        public void QuitGame() {
+            Application.Quit();
+        }
+    }
+}
